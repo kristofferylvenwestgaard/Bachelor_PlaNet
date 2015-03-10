56f351b0f1bba3614c5c3433fbf13737c1ea245f
magnus87 <magnus.elde@gmail.com>
2015-03-10 12:52:55 +0100

    Siste commit etter møte 10.03.2015

--- PlaNet_NetApp/includes/top.php
+++ PlaNet_NetApp/includes/top.php
@@ -3,6 +3,7 @@
 	<meta charset="UTF-8"/>
 	<title>PlaNet</title>
 	<link rel="stylesheet" media="screen" type="text/css" href="css/style.css"/>
+	<script type="text/javascript" src="bildevindu.js"></script>
 </head>
 <body>
 <?php
---
+++ PlaNet_NetApp/javascript/bildevindu.js
@@ -0,0 +1,12 @@
+<script>
+var myWindow;
+
+function openWin() {
+    myWindow = window.open("", "myWindow", "width=200, height=100");
+    myWindow.document.write("<p>This is 'myWindow'</p>");
+}
+
+function closeWin() {
+    myWindow.close();
+}
+</script>

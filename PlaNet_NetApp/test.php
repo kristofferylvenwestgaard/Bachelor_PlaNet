0eeb0cd6720fa02be8d41e7247d415cf320c37ab
Kristoffer Ylven Westgaard <s189143@stud.hioa.no>
2015-04-01 16:28:48 +0200

    s

--- PlaNet_NetApp/test.php
+++ PlaNet_NetApp/test.php
@@ -1,14 +1,13 @@
 <?php
 include "includes/top.php";
 	include "includes/footer.php";
-	
+
 
 	$_SESSION['aid'] = hentAktivitetId();
 	$stegNummer = 1;
 	//LEGGE INN HANDLINGSKJEDESTEG MED ID LIK SESSION["aid"]
 	if(isset($_REQUEST['leggTil']))
 	{
-		echo "hei";
 		if(isset($_REQUEST['handlingskjede']))
 		{
 			addHandlingSteg($_SESSION['aid'], $_REQUEST['beskrivelseBoks'], $stegNummer);

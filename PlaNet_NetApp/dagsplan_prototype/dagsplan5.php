29aec660bdd13e438c7c5cab9256f0c39426f970
Kristoffer Ylven Westgaard <s189143@stud.hioa.no>
2015-04-28 17:28:03 +0200

    Forberedt Dagsplan-sider
    Har lagd duplikater som er klare for å plott inn når vi får detaljert
    info på mandag!

---
+++ PlaNet_NetApp/dagsplan_prototype/dagsplan5.php
@@ -0,0 +1,89 @@
+	<?php
+	include "includes/top.php";
+	include "includes/footer.php";
+
+?>
+
+<div class="container">
+	<!--HEADER-->
+	<div class="col-1-3">
+		<div class="header">
+			<a href="index.php"><h1 class="htext">Terje</h1></a>
+		</div>
+	</div>
+	<div class="col-1-6">
+		<div class="header">
+			
+			<a id="ukemndknapper" href="index.php" class="btn btn-large btn-block btn-outline sharp" type="button">Uke</button></a>
+		</div>
+	</div>
+	<div class="col-1-6">
+		<div class="header">
+			<a id="ukemndknapper" href="april.php" class="btn btn-large btn-block btn-outline sharp" type="button">Måned</button></a>
+		</div>
+	</div>
+	<div class="col-1-3">
+		<div class="header">
+			<!--Skal byttes ut med JS og Symbol!!!-->
+			<div class="col-1-1">
+				<div id="clock"></div>
+				<div id="admin">
+				<a href="admin.php"><img src="img/admin1.png" width="45px" height="45px" alt="admin"/></a>
+				</div>
+			</div>
+		</div>
+	</div>
+<div class = "container">
+    <div class="col-2-12">
+           <div id="dagIdagsPlan">Torsdag</div>
+    </div>
+    <div class="col-8-12">
+           <div class="aktivitetNavn">List ut aktivtetsNavn</div>
+    </div>
+    <div class="col-2-12">
+           <div class="stegTittel">Steg</div>
+    </div>
+</div>
+
+<div class = "container">
+	
+		<div class="col-2-12">
+				<div id="torsdag">
+					<?php	
+						hentAktivitet();
+						//LEGGE TIL KNAPP FOR Å VELGE HVILKEN AKTIVITET MAN SKAL GJENNOMFØRE?
+						//--------> HER <---------//
+						//KNAPPEN HENTER RIKTIG AKTIVITETSID OG TILEGNER DEN I $aktivitetId under.
+					?>
+				</div>
+		</div>
+		<div class="col-8-12">
+	           <div class="grid">
+	           		<div class="dagsplanBoks">
+	           		
+	           		<!--STATISK INFORMASJON IFMA HTML CSS-->
+	           		<!--HANDLINGSKJEDESTEG!-->
+
+						
+					</div>
+	           </div>
+	     </div>
+	     <div class="col-2-12">
+	     	<div class="dagsChecklist">
+
+	     			<!--HYPERLINKS TIL NESTE HANDLINGSSTEG!-->
+	     			<input class="submit btn btn-xl btn-block btn-success sharp" type="submit" value="1" name="check"/>
+
+	     			<input class="submit btn btn-xl btn-block btn-success sharp" type="submit" value="✓" name="check"/>
+	     		
+	     			<input class="submit btn btn-xl btn-block btn-success sharp" type="submit" value="3" name="check"/>
+	     		
+	     			<input class="submit btn btn-xl btn-block btn-success sharp" type="submit" value="4" name="check"/>
+
+	     			<input class="submit btn btn-xl btn-block btn-success sharp" type="submit" value="5" name="check"/>
+	     		
+	     			<input class="submit btn btn-xl btn-block btn-success sharp" type="submit" value="6" name="check"/>
+	     	</div>
+	     </div>
+	
+ </div>

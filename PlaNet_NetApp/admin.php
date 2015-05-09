f49b474f43e6a2700cfcdb55e69ad2a60cde61f5
magnus87 <magnus.elde@gmail.com>
2015-05-10 00:55:01 +0200

    das

--- PlaNet_NetApp/admin.php
+++ PlaNet_NetApp/admin.php
@@ -6,18 +6,28 @@
 	<!--HEADER-->
 	<div class="col-1-3">
 		<div class="header">
-			<h1 class="htext">Terje</h1>
+			<h1 class="htext">
+				<a href=admin.php>
+				<i class="fa fa-user"></i>
+				Terje
+			</a>
+			</h1>
 		</div>
 	</div>
 	<div class="col-1-6">
 		<div class="header">
 			
-			<a id="ukemndknapper" href="index.php" class="btn btn-large btn-block btn-outline sharp" type="button">Uke</button></a>
+			<a id="ukemndknapper" href="index.php" class="btn btn-large btn-block btn-outline sharp" type="button">
+				<i class="fa fa-columns"></i>  Uke</button>
+			</a>
 		</div>
 	</div>
 	<div class="col-1-6">
 		<div class="header">
-			<a id="ukemndknapper" href="april.php" class="btn btn-large btn-block btn-outline sharp" type="button">Måned</button></a>
+			<a id="ukemndknapper" href="mai.php" class="btn btn-large btn-block btn-outline sharp" type="button">
+
+				<i class="fa fa-calendar-o"></i>  Måned</button>
+			</a>
 		</div>
 	</div>
 	<div class="col-1-3">
@@ -26,7 +36,7 @@
 			<div class="col-1-1">
 				<div id="clock"></div>
 				<div id="admin">
-					<a href="admin.php"><img src="img/admin1.png" width="45px" height="45px" alt="admin"/></a>
+					<a href="admin.php"><i class="fa fa-cog fa-3x"></i></a>
 				</div>
 			</div>
 		</div>

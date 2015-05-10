2a6b792a92dbaa976f29f95599e4e3c2e16bbe2e
magnus87 <magnus.elde@gmail.com>
2015-05-10 16:44:02 +0200

    link fra bursdag i mnd til bursdag aktivitet

---
+++ PlaNet_NetApp/aktivitetBursdag.php
@@ -0,0 +1,81 @@
+<?php
+	include "includes/top.php";
+	include "includes/footer.php";
+
+?>
+
+<div class="container">
+	<!--HEADER-->
+	<div class="col-1-3">
+		<div class="header">
+			<a href="index.php"><h1 class="htext"></h1></a>
+		</div>
+	</div>
+	<div class="col-1-6">
+		<div class="header">
+            
+            <a id="ukemndknapper" href="index.php" class="btn btn-large btn-block btn-outline sharp" type="button">
+                <i class="fa fa-columns"></i>  Uke</button>
+            </a>
+        </div>
+    </div>
+    <div class="col-1-6">
+        <div class="header">
+            <a id="ukemndknapper" href="index.php" class="btn btn-large btn-block btn-outline sharp" type="button">
+                <i class="fa fa-calendar-o"></i>  Måned</button>
+            </a>
+        </div>
+    </div>
+	<div class="col-1-3">
+		<div class="header">
+			<!--Skal byttes ut med JS og Symbol!!!-->
+			<div class="col-1-1">
+				<div id="clock"></div>
+				<div id="admin">
+				<a href="admin.php"><img src="img/admin1.png" width="45px" height="45px" alt="admin"/></a>
+				</div>
+			</div>
+		</div>
+	</div>
+<div class = "container">
+
+
+
+
+	<div class="col-1-1">
+           <div class="aktivitetNavnAktivitet">Bursdag</div>
+    </div>
+		<div class="admincontent">
+			<div class= "col-3-4">
+	           			<div class="aktBilde">
+	           			<img src="img/bursdag.jpg" width="750px" height="350px" alt="admin"/></a>
+	           			</div>
+	           		</div>
+
+					<div class= "col-1-4">
+						<div class="akttekst">
+							Besøk fra familie klokken 17:00
+						</div>
+	           		</div>
+	           		<div class="col-1-1">
+				<div class="col-1-3">
+					          
+					            <a class="btn btn-warning btn-dobbelxl btn-block sharp" href="index.php">
+					              <i class="fa fa-times"></i> Avbryt</a>
+
+					    </div>
+
+			
+
+				
+				
+				<div class="col-2-3">
+					  <a class="btn btn-primary btn-dobbelxl btn-block sharp disabled" href="index.php">
+					              <i class="fa fa-check"></i> Ferdig</a>
+					 
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

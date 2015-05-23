fe45554e459425beeb47f99640106e0a383b5682
magnus87 <magnus.elde@gmail.com>
2015-05-23 15:11:15 +0200

    Prosjektside
    URL: hovedprosjekt/prosjektside/index.php

---
+++ prosjektside/dokumenter.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Prosjektside">
+    <meta name="author" content="Gruppe 2015 - 20">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <title>PlaNet - prosjektside</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation"><a href="index.php"><i class="fa fa-home"></i> Hjem</a></li>
+            <li role="presentation" class="active"><a href="#"><i class="fa fa-file"></i> Dokumentasjon</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Prosjektside</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Dokumentasjon</h1>
+        <p class="lead">Gruppe 2015 - 20</p>
+      </br>
+        <div class="row">
+          <div class="col-md-6">
+          </br>
+        <p><a class="btn btn-lg btn-primary btn-block" href="Rapport.pdf" role="button"> <i class="fa fa-book"></i> Prosjektrapport</a></p>
+      </br>
+        <p><a class="btn btn-lg btn-primary btn-block" href="Statusrapport.pdf" role="button"> <i class="fa fa-file-pdf-o"></i> Statusrapport</a></p>
+      </div>
+    </br>
+      <div class="col-md-6">
+       <p><a class="btn btn-lg btn-primary btn-block" href="Forprosjektsrapport.pdf" role="button"> <i class="fa fa-file-pdf-o"></i> Forprosjekt</a></p>
+      </br>
+        <p><a class="btn btn-lg btn-primary btn-block" href="Prosjektskisse.pdf" role="button"> <i class="fa fa-file-pdf-o"></i> Prosjektskisse</a></p>
+      </div>
+      </div>
+    </div>
+
+ 
+
+      <footer class="footer">
+        <p>Gruppe 2015 - 20 </p> 
+          <div class="hioa">
+          <a href="http://www.hioa.no/">
+          <img src="img/hioa.png" width="220px;" alt="HiOA logo"/>
+          </a>
+        </div>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
